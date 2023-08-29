6812f823bee32d771d2804cf377c59171fb65633
dinobotry <dino@robotry.co.kr>
2023-08-29 13:40:31 +0900

    프로젝트 생성

---
+++ src/test/java/com/study/security/secutirystudy/SecutiryStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.security.secutirystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecutiryStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

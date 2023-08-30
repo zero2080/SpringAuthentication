82f1fde2943cac7adfb7c2789187c05de08c61ff
dinobotry <dino@robotry.co.kr>
2023-08-30 22:47:28 +0900

    JWT토큰 발급

--- src/main/java/com/study/security/authentication/JwtProvider.java
+++ src/main/java/com/study/security/authentication/JwtProvider.java
@@ -5,47 +5,46 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.time.Instant;
-import java.util.Base64;
 import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class JwtProvider {
-  private final String SECRET = Base64.getEncoder().encodeToString("json-web-token-secret-key-algorithm-bit-same-length".getBytes());
-  private final int EXPIRATION_TIME = 60*60*24;
+  private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
-  public String generateToken(Customer customer){
+  public String generateToken(Customer customer) {
 
-
-    Claims claims = Jwts.claims().setSubject(customer.getLoginId()); // JWT payload 에 저장되는 정보단위
+    Claims claims = Jwts.claims().setSubject(String.valueOf(customer.getId())); // JWT payload 에 저장되는 정보단위
     claims.put("roles", "ROLE_USER");
     Date now = Date.from(Instant.now());
     return Jwts.builder()
         .setClaims(claims)
         .setIssuedAt(now)
-        .setExpiration(new Date(now.getTime()+EXPIRATION_TIME))
-        .signWith(SignatureAlgorithm.HS256, SECRET)
+        .setExpiration(new Date(now.getTime() + (60 * 60 * 24)))
+        .signWith(SECRET_KEY)
         .compact();
   }
 
-  public String getUsernameFromToken(String token){
+  public String getUsernameFromToken(String token) {
     return null;
   }
 
-  public boolean validateToken(String token){
+  public boolean validateToken(String token) {
     try {
-      Jws<Claims> claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+      Jws<Claims> claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
       return claims.getBody().getExpiration().toInstant().isAfter(Instant.now());
-    }finally{
+    } finally {
       return false;
     }
   }
 
-  public String resolveToken(String token){
+  public String resolveToken(String token) {
     return null;
   }
 }
